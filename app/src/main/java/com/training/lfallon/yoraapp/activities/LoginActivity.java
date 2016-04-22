33ea7d050e760dbc6cb2914e13b9ee661d02b8b7
Lance Fallon <lfallon@Lances-Mac-mini.local>
2016-04-22 10:23:10 -0400

    wire up loginActivity to register and external login activities

--- app/src/main/java/com/training/lfallon/yoraapp/activities/LoginActivity.java
+++ app/src/main/java/com/training/lfallon/yoraapp/activities/LoginActivity.java
@@ -15,8 +15,13 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
 
     private static final String tag = LoginActivity.class.getSimpleName();
     private static final int REQUEST_NARROW_LOGIN = 1;
+    private static final int REQUEST_EXTERNAL_LOGIN = 2;
+    private static final int REQUEST_REGISTER = 3;
 
     private View loginButton;
+    private View registerButton;
+    private View facebookButton;
+    private View googleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +33,33 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
         if(loginButton != null){
             loginButton.setOnClickListener(this);
         }
+
+        registerButton = findViewById(R.id.activity_login_register);
+        facebookButton = findViewById(R.id.activity_login_facebook);
+        googleButton = findViewById(R.id.activity_login_google);
+
+        registerButton.setOnClickListener(this);
+        facebookButton.setOnClickListener(this);
+        googleButton.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        if(v == loginButton){
+    public void onClick(View view) {
+        if(view == loginButton) {
             startActivityForResult(new Intent(this, LoginNarrowActivity.class), REQUEST_NARROW_LOGIN);
+        }
+        else if (view == registerButton){
+            startActivityForResult(new Intent(this, RegisterActivity.class), REQUEST_REGISTER);
+        }
+        else if (view == facebookButton){
+            Intent intent = new Intent(this, ExternalLoginActivity.class);
+            intent.putExtra(ExternalLoginActivity.EXTRA_EXTERNAL_SERVICE, "Facebook");
+            startActivityForResult(intent, REQUEST_EXTERNAL_LOGIN);
+        }
+        else if (view == googleButton) {
+            Intent intent = new Intent(this, ExternalLoginActivity.class);
+            intent.putExtra(ExternalLoginActivity.EXTRA_EXTERNAL_SERVICE, "Google");
+            startActivityForResult(intent, REQUEST_EXTERNAL_LOGIN);
         }
     }
 
@@ -43,7 +69,9 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
         if(resultCode != RESULT_OK)
             return;
 
-        if(requestCode == REQUEST_NARROW_LOGIN){
+        if(requestCode == REQUEST_NARROW_LOGIN
+                || requestCode == REQUEST_EXTERNAL_LOGIN
+                || requestCode == REQUEST_REGISTER){
             finishLogin();
         }
     }
